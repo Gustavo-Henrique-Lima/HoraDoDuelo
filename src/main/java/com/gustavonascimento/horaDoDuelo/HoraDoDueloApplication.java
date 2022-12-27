c643c04c46c44f1fa935933cd35888560f721aa4
Gustavo Henrique <gustavohgustavo@hotmail.com>
2022-12-27 12:01:08 -0300

    Projeto criado

---
+++ src/main/java/com/gustavonascimento/horaDoDuelo/HoraDoDueloApplication.java
@@ -0,0 +1,13 @@
+package com.gustavonascimento.horaDoDuelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoraDoDueloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoraDoDueloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustavonascimento/horaDoDuelo/HoraDoDueloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavonascimento.horaDoDuelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoraDoDueloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
